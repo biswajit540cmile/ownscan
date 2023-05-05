139713564128594d8d5945e7bc5888e2d468f093
biswajit540cmile <biswajit.samal@cmile.in>
2023-05-05 19:20:52 +0530

    push my code

---
+++ android/app/src/main/kotlin/com/example/impscan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.impscan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/scanresult.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+// class Resultpage extends StatelessWidget {
+//   Resultpage(scanBarcode, {super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Center(
+//             child: Text('Scan result : ${Resultpage} \n',
+//                 style: TextStyle(fontSize: 20))));
+//   }
+// }
+class Resultpage extends StatelessWidget {
+  String? scanBarcode;
+  Resultpage(this.scanBarcode);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        // title: Text('My Screen'),
+      ),
+      body: Center(
+        child: Text("Bar Code Number:$scanBarcode"),
+      ),
+    );
+  }
+}
